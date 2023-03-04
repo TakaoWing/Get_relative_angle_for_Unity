6fb2c37e43858588901d4c2a066ac18e011549eb
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2023-03-04 14:02:08 +0900

    プログラムをよりシンプルなものに変更

--- Assets/EnemyDirection.cs
+++ Assets/EnemyDirection.cs
@@ -4,8 +4,7 @@ using UnityEngine.UI;
 public class EnemyDirection : MonoBehaviour
 {
     [SerializeField] private Transform enemyTransform;
-    [SerializeField] private float leftAngleThreshold = -45f;
-    [SerializeField] private float rightAngleThreshold = 45f;
+    [SerializeField] private float angleThreshold = 45f;
     [SerializeField] private Text directionText;
 
     private void Update()
@@ -13,17 +12,20 @@ public class EnemyDirection : MonoBehaviour
         Vector3 directionToEnemy = enemyTransform.position - transform.position;
         float angleToEnemy = Vector3.SignedAngle(transform.forward, directionToEnemy, Vector3.up);
 
-        if (angleToEnemy > rightAngleThreshold)
+        if (Mathf.Abs(angleToEnemy) > angleThreshold)
         {
-            directionText.text = "Enemy is on the right side of the player.";
-        }
-        else if (angleToEnemy < leftAngleThreshold)
-        {
-            directionText.text = "Enemy is on the left side of the player.";
+            if (angleToEnemy > 0f)
+            {
+                directionText.text = "Enemy is on the right.";
+            }
+            else
+            {
+                directionText.text = "Enemy is on the left.";
+            }
         }
         else
         {
-            directionText.text = "Enemy is in front of or behind the player.";
+            directionText.text = "";
         }
     }
 }
